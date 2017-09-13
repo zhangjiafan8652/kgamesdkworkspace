2815b054a95de313bd12cd4cde565691e44d17d4
365449552@qq.com <365449552@qq.com>
2017-09-13 14:01:51 +0800

    11111111111111111111111111

---
+++ SDKyywDgamelib/src/com/ddgame/impl/YYApplication.java
@@ -0,0 +1,14 @@
+package com.ddgame.impl;
+
+import com.ddgame.proxy.YYWApplication;
+
+public class YYApplication extends YYWApplication {
+
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		
+	}
+}
